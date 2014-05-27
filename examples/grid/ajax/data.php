637789c2cc01a3ac25bdef41fc3603aa6e09e418
Cesar Nunton <cnunton@dasoft.ca>
2014-05-27 09:08:51 -0500

    Subida de Libreria EXT JS

---
+++ examples/grid/ajax/data.php
@@ -0,0 +1,16 @@
+<?php
+$rsData[0]['title']='title1';
+$rsData[0]['threadid']='133690';
+$rsData[0]['username']='133690';
+$rsData[0]['userid']='133690';
+$rsData[0]['dateline']='133690';
+$rsData[0]['postid']='133690';
+$rsData[0]['forumtitle']='133690';
+$rsData[0]['forumid']='133690';
+$rsData[0]['replycount']='133690';
+$rsData[0]['lastpost']='133690';
+$rsData[0]['lastposter']='133690';
+$rsData[0]['excerpt']='133690';
+$res = array('totalCount' => 6679, 'topics' => array_splice($rsData, 0, 1));
+echo json_encode($res);
+?>
